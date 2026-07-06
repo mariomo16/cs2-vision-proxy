e6f4e9e73be4b7e32ff8f734199cdfc96b129e9b
mariomo16 <mariomo16.cieza@gmail.com>
2026-07-06 17:55:28 +0200

    refactor: remove unnecessary comments and clean up CSS for route hover

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const contents = document.querySelectorAll(".tab-content");
 
 for (const btn of tabs) {
   btn.addEventListener("click", () => {
-    // Limpiar clases
     for (const b of tabs) b.classList.remove("active");
     for (const c of contents) c.classList.remove("active");
 
-    // Activar seleccionados
     btn.classList.add("active");
     const target = document.querySelector(
       `.tab-content[data-tab="${btn.dataset.tab}"]`,
